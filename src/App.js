dd59b4d44ff68299b3ab8c5748966dfd160ff005
yehiagamalx <yehiagamaleg@gmail.com>
2022-01-16 01:20:27 +0200

    fix bugs commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     );
     saveLocalTodos();
     filterHandler();
-  }, [todoitemsAreHere, filterTheView]);
+  }, [todoitemsAreHere, filterTheView, filterHandler, saveLocalTodos]);
 
   // local
 
